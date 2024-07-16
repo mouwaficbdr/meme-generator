7f742e01d558a8502ec7d5b0c8180a7e721fdd65
Mouwafic Badarou <badaroumouwafic@gmail.com>
2024-07-16 18:13:11 +0100

    Header Component + Meme component

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import logoIcon from "../assets/troll-face.png"
+
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <img src={logoIcon} className="logo-icon" />
+        <p className="logo-text">Meme Generator</p>
+      </div>
+      <span className="course-name">React Course - Project 3</span>
+    </header>
+  );
+}
